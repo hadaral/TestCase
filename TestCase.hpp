f0365be67c667f4341444c5deec300a1217c9234
hadaral <33652545+hadaral@users.noreply.github.com>
2018-06-11 17:40:17 +0300

    Add files via upload

--- TestCase.hpp
+++ TestCase.hpp
@@ -8,53 +8,54 @@ using namespace std;
 
 class TestCase{
     public:
-        string msg;
+        //string msg;
         ostream& out;
 
         int failed, passed, total;
 
         TestCase (string nameOfCheck , ostream& os): out(os),failed(0),passed(0),total(0){
-            msg = nameOfCheck;
+            out << nameOfCheck;
+            //msg = nameOfCheck;
         }
 
-        template <typename T> void check_equal(T x, T y){
+        template <typename T> TestCase& check_equal(T x, T y){
             total ++;
             if (x==y){
                 passed ++;
             }
             else{
                 failed ++;
-                out << msg << " Failure in test #" << total << ":" << x << " should equal " << y << "!" << endl;
+                out <<  ": Failure in test #" << total << ":" << x << " should equal " << y << "!" << endl;
             }
-            // return *this;
+            return *this;
         }
 
-        template <typename T> void check_different(T x, T y){
+        template <typename T> TestCase& check_different(T x, T y){
             total++;
             if (x!=y){
                 passed ++;
             }
             else{
-                out << msg << " Failure in test #" << total << ":" << x << " should different " << y << "!" <<endl; 
+                out << ": Failure in test #" << total << ":" << x << " should different " << y << "!" <<endl; 
                 failed ++;
             }
-            // return *this;
+            return *this;
         }
         
-        template <typename T, typename Fun> void check_function (Fun func, T x, int y){
+        template <typename T, typename Fun> TestCase& check_function (Fun func, T x, int y){
             total ++;
             if (func(x) == y){
                 passed ++;
             }
             else{
                 failed ++;
-                out << msg << " Failure in test #" << total << ": Function should return " << y << " but returned " << func(x) << "!" <<endl; 
+                out << ": Failure in test #" << total << ": Function should return " << y << " but returned " << func(x) << "!" <<endl; 
 
             }
-            // return *this;
+            return *this;
         }
 
-        template <typename T> void check_output( T a, string y){
+        template <typename T> TestCase& check_output( T a, string y){
             total++;
             ostringstream ss;
             ss<<a;
@@ -63,14 +64,14 @@ class TestCase{
             }
             else{
                 failed++;
-                out << msg << " Failure in test #" << total << ": string value should be " << y << " but is " << ss.str() << endl;
+                out << ": Failure in test #" << total << ": string value should be " << y << " but is " << ss.str() << endl;
             }
-            // return *this;
+            return *this;
         }
 
 
         void print(){
-            out << msg << failed << " failed, " << passed << " passed, " << total << "total." << endl;
+            out << failed << " failed, " << passed << " passed, " << total << " total." << endl << "---" << endl;
         }
 
 
